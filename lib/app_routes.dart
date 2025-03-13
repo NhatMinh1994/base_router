290f93235b89a9dcea9eb643e27a4d2093098299
minhnn1 <minhnn1@vpbanks.com.vn>
2025-03-13 15:12:14 +0700

    update path

---
+++ lib/app_routes.dart
@@ -0,0 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+class AppRoutes {
+  static const String initialPath = '/';
+  static const String demoscreen2 = '/demoScreen2';
+  static const String exampleanimation = '/exampleAnimation';
+  static const String firstscreen = '/firstScreen';
+  static const String secondscreen = '/secondScreen';
+  static const String feature2FirstScreen = '/feature2/firstScreen';
+  static const String feature2SecondScreen = '/feature2/secondScreen';
+}
